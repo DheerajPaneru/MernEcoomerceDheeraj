9a2c9772dcc26f09d7af7f87c151ca1f90a8ca36
Dheeraj Paneru <dheerajpaneru8@gmail.com>
2023-10-12 09:17:56 +0530

    changes in env file

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+mongoose.set("strictQuery", false);
+
+
+mongoose.connect("mongodb://localhost:27017/5678",{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    family:4 
+}).then(()=> console.log("DataBase Connected")).catch((errr)=>{
+    console.log(errr);
+})
